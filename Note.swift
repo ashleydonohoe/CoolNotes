3dd41dc2dab54fbba1a0f4fa7992dd12fa095dea
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-09-01 23:44:13 -0400

    Added convenience init for Note

--- Note.swift
+++ Note.swift
@@ -13,5 +13,25 @@ import CoreData
 class Note: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
-
+    convenience init(text: String = "New Note", context: NSManagedObjectContext) {
+        if let ent = NSEntityDescription.entityForName("Note", inManagedObjectContext: context){
+            self.init(entity: ent, insertIntoManagedObjectContext: context)
+            self.text = text
+            self.creationDate = NSDate()
+        } else {
+            fatalError("Unable to find Entity name")
+        }
+    }
+    
+    var humanReadableAge: String {
+        get {
+            let fmt = NSDateFormatter()
+            fmt.timeStyle = .NoStyle
+            fmt.dateStyle = .ShortStyle
+            fmt.doesRelativeDateFormatting = true
+            fmt.locale = NSLocale.currentLocale()
+            
+            return fmt.stringFromDate(creationDate!)
+        }
+    }
 }
