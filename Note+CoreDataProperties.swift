efbce971a77c272d68384a6f202206e9ce5f232c
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-09-01 23:39:53 -0400

    Generated CoreData files

--- Note+CoreDataProperties.swift
+++ Note+CoreDataProperties.swift
@@ -1,9 +1,9 @@
 //
 //  Note+CoreDataProperties.swift
-//  CoolNotes
+//  Cool Notes
 //
-//  Created by Fernando Rodríguez Romero on 10/03/16.
-//  Copyright © 2016 udacity.com. All rights reserved.
+//  Created by Gabriele on 9/1/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -13,9 +13,9 @@ import Foundation
 import CoreData
 
 extension Note {
-    
-    @NSManaged var creationDate: NSDate?
+
     @NSManaged var text: String?
+    @NSManaged var creationDate: NSDate?
     @NSManaged var notebook: NSManagedObject?
-    
+
 }
---
+++ Note.swift
@@ -0,0 +1,17 @@
+//
+//  Note.swift
+//  Cool Notes
+//
+//  Created by Gabriele on 9/1/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Note: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
